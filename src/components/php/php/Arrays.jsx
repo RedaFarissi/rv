3e74bc4b3845c90fbc81dcf477b32886fb733d01
RedaFarissi <redaesskouni@gmail.com>
2024-02-20 20:21:38 +0100

    php

--- src/components/php/php/Arrays.jsx
+++ src/components/php/php/Arrays.jsx
@@ -6,8 +6,127 @@ export default function Arrays(props){
         <h1 className="heading-style"> PHP Arrays </h1>
         <article className="mt-5">
             <p className="style_divv">
-                
+                في <b>PHP</b>، الـ <b>Arrays</b> تُستخدم لتخزين مجموعة من القيم تحت اسم واحد. يمكن أن تحتوي الـ <b>Arrays</b> على أنواع مختلفة من البيانات، مثل الأعداد الصحيحة، والسلاسل النصية، والأصناف الأخرى  
             </p>
+            <h2 className="title-h2">1 - إنشاء Array</h2>
+            <CodeHighlighter code={`<?php
+    // Create an array for colors
+    $colors = array("red", "green", "blue");
+
+    // Alternatively, create an array using square brackets []
+    $colors = ["red", "green", "blue"];
+?>`} language="php" addclassName="mt-3 mb-3" copie={true}/>
+            <h2 className="title-h2">2 - الوصول إلى العناصر في الـ Array </h2>
+            <CodeHighlighter code={`<?php
+    // Create an array for colors
+    $colors = array("red", "green", "blue");
+
+    // Accessing the element using the key (index)
+    echo $colors[0]; // Prints "red"
+
+?>`} language="php" file_name="C:\xampp\htdocs\php-test\index.php" addclassName="mt-3 mb-3" copie={true}/>
+            <Result title="localhost/php-test/index.php" file_name="index.html" logo={props.xampp_logo}  route="http://localhost/php-test/index.php">
+                red
+            </Result>
+            <h2 className="title-h2">3 - إضافة عنصر جديد إلى الـ Array </h2>
+            <CodeHighlighter code={`<?php
+    // Create an array for colors
+    $colors = array("red", "green", "blue");
+
+    // Adding a new element to the end
+    $colors[] = "yellow";
+?>
+
+<pre><?php var_dump($colors); ?></pre>`} language="php"  file_name="C:\xampp\htdocs\php-test\index.php" addclassName="mt-3 mb-3" copie={true}/>
+            <Result title="localhost/php-test/index.php" file_name="index.html" logo={props.xampp_logo}  route="http://localhost/php-test/index.php">
+                array(4) {"{"}<br/>
+                    &nbsp;&nbsp; [0]={">"}<br/>
+                    &nbsp;&nbsp; string(3) "red"<br/>
+                    &nbsp;&nbsp; [1]={">"}<br/>
+                    &nbsp;&nbsp; string(5) "green"<br/>
+                    &nbsp;&nbsp; [2]={">"}<br/>
+                    &nbsp;&nbsp; string(4) "blue"<br/>
+                    &nbsp;&nbsp; [3]={">"}<br/>
+                    &nbsp;&nbsp; string(6) "yellow"<br/>
+                {"}"}<br/>
+            </Result>
+            <h2 className="title-h2">4 -  عد عناصر المصفوفة </h2>
+            <CodeHighlighter code={`<?php
+    // Create an array for colors
+    $colors = array("red", "green", "blue");
+
+    // Adding a new element to the end
+    $colors[] = "yellow";
+?>
+
+<h2><?php echo "Count Array Items : ".count($colors); ?></h2>`} language="php" file_name="C:\xampp\htdocs\php-test\index.php" addclassName="mt-3 mb-3" copie={true}/>
+            <Result title="localhost/php-test/index.php" file_name="index.html" logo={props.xampp_logo}  route="http://localhost/php-test/index.php">
+                <h2>Count Array Items : 4</h2>
+            </Result>
+            <h2 className="title-h2">5 - طباعة جميع العناصر في الـ Array</h2>
+            <CodeHighlighter code={`<?php
+    // Create an array for colors
+    $colors = array("red", "green", "blue");
+  
+    // Adding a new element to the end
+    $colors[] = "yellow";
+  
+    // Printing all elements using foreach
+    foreach ($colors as $color) {
+      echo "<h2>" . $color . "</h2>";
+    }
+?>`} language="php" file_name="C:\xampp\htdocs\php-test\index.php" addclassName="mt-3 mb-3" copie={true}/>
+            <Result title="localhost/php-test/index.php" file_name="index.html" logo={props.xampp_logo}  route="http://localhost/php-test/index.php">
+                <h2>red</h2>
+                <h2>green</h2>
+                <h2>blue</h2>
+                <h2>yellow</h2>
+            </Result>
+            <h2 className="title-h2">6 - Arrays بمفاتيح </h2>
+            <CodeHighlighter code={`<?php
+    // Creating an associative array to store person information
+    $person = array(
+        "name" => "Reda Eskouni",
+        "age" => 30,
+        "city" => "Sidi Slimane"
+    );
+
+    // Accessing elements using keys
+    echo "<h2>" . $person["name"] . "</h2>"; 
+    $person["age"] =  27;
+    echo "<h2>" . $person["age"] . "</h2>"; 
+?>`} language="php" file_name="C:\xampp\htdocs\php-test\index.php" addclassName="mt-3 mb-3" copie={true}/>
+            <Result title="localhost/php-test/index.php" file_name="index.html" logo={props.xampp_logo}  route="http://localhost/php-test/index.php">
+                <h2>Reda Eskouni</h2>
+                <h2>27</h2>
+            </Result> 
+            <h2 className="title-h2">7 - مزيد من العمليات على الـ Arrays </h2>
+            <CodeHighlighter code={`<?php
+    // Create an array for colors
+    $colors = array("red", "green", "blue");
+  
+    // Adding an element to the end
+    array_push($colors, "purple");
+
+    echo "<h2>". $colors[3] ."</h2>";
+
+    // Removing an element from the end
+    $lastColor = array_pop($colors);
+
+    echo "<h2>". $lastColor ."</h2>";
+?>`} language="php" file_name="C:\xampp\htdocs\php-test\index.php" addclassName="mt-3 mb-3" copie={true}/>
+            <Result title="localhost/php-test/index.php" file_name="index.html" logo={props.xampp_logo}  route="http://localhost/php-test/index.php">
+                <h2>purple</h2>
+                <h2>purple</h2>
+                array(3) {"{"}<br/>
+                  &nbsp; &nbsp; [0]={">"}<br/>
+                  &nbsp; &nbsp; string(3) "red"<br/>
+                  &nbsp; &nbsp; [1]={">"}<br/>
+                  &nbsp; &nbsp; string(5) "green"<br/>
+                  &nbsp; &nbsp; [2]={">"}<br/>
+                  &nbsp; &nbsp; string(4) "blue"<br/>
+                {"}"}
+            </Result>
         </article>
     </>
     )
--- src/components/php/php/Superglobals.jsx
+++ src/components/php/php/Superglobals.jsx
@@ -5,9 +5,118 @@ export default function Superglobals(props){
     <>
         <h1 className="heading-style"> PHP Superglobals </h1>
         <article className="mt-5">
+            <details  className="mt-5 my-4">
+                <summary className='text-danger'> جميع العناوين  </summary>
+                <ul dir="ltr">
+                    <li><a className="text-decoration-none" href="#GLOBALS"> $GLOBALS </a></li>
+                    <li><a className="text-decoration-none" href="#SERVER"> $_SERVER </a></li>
+                    <li><a className="text-decoration-none" href="#REQUEST"> $_REQUEST </a></li>
+                    <li><a className="text-decoration-none" href="#POST"> $_POST </a></li>
+                    <li><a className="text-decoration-none" href="#GET"> $_GET </a></li>
+                    <li><a className="text-decoration-none" href="#FILES"> $_FILES </a></li>
+                    <li><a className="text-decoration-none" href="#ENV"> $_ENV </a></li>
+                    <li><a className="text-decoration-none" href="#COOKIE"> $_COOKIE </a></li>
+                    <li><a className="text-decoration-none" href="#SESSION"> $_SESSION </a></li>
+                </ul>
+            </details>
             <p className="style_divv">
-                
+                بعض المتغيرات المحددة مسبقًا في <b>PHP</b> هي <b>"superglobals"</b>، مما يعني أنه يمكن الوصول إليها دائمًا، بغض النظر عن النطاق - ويمكنك الوصول إليها من أي وظيفة أو فئة أو ملف دون الحاجة إلى القيام بأي شيء خاص.<br/><br/>
+                المتغيرات <b>PHP superglobal</b> هي: <br/>
+                <ul dir="ltr">
+                    <li><bdi><b>$GLOBALS</b></bdi></li>
+                    <li><bdi><b>$_SERVER</b></bdi></li>
+                    <li><bdi><b>$_REQUEST</b></bdi></li>
+                    <li><bdi><b>$_POST</b></bdi></li>
+                    <li><bdi><b>$_GET</b></bdi></li>
+                    <li><bdi><b>$_FILES</b></bdi></li>
+                    <li><bdi><b>$_ENV</b></bdi></li>
+                    <li><bdi><b>$_COOKIE</b></bdi></li>
+                    <li><bdi><b>$_SESSION</b></bdi></li>
+                </ul>
             </p>
+            <h2 className="title-h2" id="GLOBALS">1 - <bdi>$GLOBALS</bdi></h2>
+            <p className="style_divv">
+                <bdi><b>$GLOBALS</b></bdi> عبارة عن مصفوفة تحتوي على كافة المتغيرات العامة.<br/><br/>
+                المتغيرات العامة هي متغيرات يمكن الوصول إليها من أي نطاق.<br/>
+                المتغيرات من النطاق الخارجي هي متغيرات عمومية تلقائيًا، ويمكن استخدامها من قبل أي نطاق، على سبيل المثال. داخل وظيفة.<br/>
+                لاستخدام متغير عام داخل دالة، عليك إما تعريفها على أنها عمومية باستخدام الكلمة الأساسية العامة، أو الرجوع إليها باستخدام بناء جملة <bdi><b>$GLOBALS</b></bdi>.
+            </p>
+            <div className="mital"> متال : </div>
+            <CodeHighlighter code={`<?php
+    $x = 75;
+
+    function myfunction() {
+        echo "<h2>". $GLOBALS['x'] ."</h2>";
+    }
+
+    myfunction();
+?>`} language="php" file_name="C:\xampp\htdocs\php-test\index.php" addclassName="mt-3 mb-3" copie={true}/>
+            <Result title="localhost/php-test/index.php" file_name="index.html" logo={props.xampp_logo}  route="http://localhost/php-test/index.php">
+                <h2>75</h2>
+            </Result>
+            <h2 className="title-h2" id="SERVER">2 - <bdi>$_SERVER</bdi></h2>
+            <p className="style_divv">
+                <bdi><b>$_SERVER</b></bdi> هو متغير عالمي فائق <b>PHP</b> يحتوي على معلومات حول الرؤوس والمسارات ومواقع البرامج النصية.<br/>
+                يوضح المثال أدناه كيفية استخدام بعض العناصر في <bdi><b>$_SERVER</b></bdi>
+            </p>
+            <div className="mital"> متال : </div>
+            <CodeHighlighter code={`<?php
+    echo "<h2>".$_SERVER['PHP_SELF']."</h2>";
+    echo "<h2>".$_SERVER['SERVER_NAME']."</h2>";
+    echo "<h2>".$_SERVER['HTTP_HOST']."</h2>";
+    echo "<h2>".$_SERVER['HTTP_USER_AGENT']."</h2>";
+    echo "<h2>".$_SERVER['SCRIPT_NAME']."</h2>";
+?>`} language="php" file_name="C:\xampp\htdocs\php-test\index.php" addclassName="mt-3 mb-3" copie={true}/>
+            <Result title="localhost/php-test/index.php" file_name="index.html" logo={props.xampp_logo}  route="http://localhost/php-test/index.php">
+                <h2>/php-test/index.php</h2>
+                <h2>localhost</h2>
+                <h2>localhost</h2>
+                <h2>Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36</h2>
+                <h2>/php-test/index.php</h2>
+            </Result>
+            
+            <h2 className="title-h2" id="REQUEST">3 - <bdi>$_REQUEST</bdi></h2>
+            <p className="style_divv"></p>
+            <div className="mital"> متال : </div>
+            <CodeHighlighter code={``} language="php" file_name="C:\xampp\htdocs\php-test\index.php" addclassName="mt-3 mb-3" copie={true}/>
+            <Result title="localhost/php-test/index.php" file_name="index.html" logo={props.xampp_logo}  route="http://localhost/php-test/index.php">
+            </Result>
+            <h2 className="title-h2" id="POST">4 - <bdi>$_POST</bdi></h2>
+            <p className="style_divv"></p>
+            <div className="mital"> متال : </div>
+            <CodeHighlighter code={``} language="php" file_name="C:\xampp\htdocs\php-test\index.php" addclassName="mt-3 mb-3" copie={true}/>
+            <Result title="localhost/php-test/index.php" file_name="index.html" logo={props.xampp_logo}  route="http://localhost/php-test/index.php">
+            </Result>
+            <h2 className="title-h2" id="GET">5 - <bdi>$_GET</bdi></h2>
+            <p className="style_divv"></p>
+            <div className="mital"> متال : </div>
+            <CodeHighlighter code={``} language="php" file_name="C:\xampp\htdocs\php-test\index.php" addclassName="mt-3 mb-3" copie={true}/>
+            <Result title="localhost/php-test/index.php" file_name="index.html" logo={props.xampp_logo}  route="http://localhost/php-test/index.php">
+            </Result>
+            <h2 className="title-h2" id="FILES">6 - <bdi>$_FILES</bdi></h2>
+            <p className="style_divv"></p>
+            <div className="mital"> متال : </div>
+            <CodeHighlighter code={``} language="php" file_name="C:\xampp\htdocs\php-test\index.php" addclassName="mt-3 mb-3" copie={true}/>
+            <Result title="localhost/php-test/index.php" file_name="index.html" logo={props.xampp_logo}  route="http://localhost/php-test/index.php">
+            </Result>
+            <h2 className="title-h2" id="ENV">7 - <bdi>$_ENV</bdi></h2>
+            <p className="style_divv"></p>
+            <div className="mital"> متال : </div>
+            <CodeHighlighter code={``} language="php" file_name="C:\xampp\htdocs\php-test\index.php" addclassName="mt-3 mb-3" copie={true}/>
+            <Result title="localhost/php-test/index.php" file_name="index.html" logo={props.xampp_logo}  route="http://localhost/php-test/index.php">
+            </Result>
+            <h2 className="title-h2" id="COOKIE">8 - <bdi>$_COOKIE</bdi></h2>
+            <p className="style_divv"></p>
+            <div className="mital"> متال : </div>
+            <CodeHighlighter code={``} language="php" file_name="C:\xampp\htdocs\php-test\index.php" addclassName="mt-3 mb-3" copie={true}/>
+            <Result title="localhost/php-test/index.php" file_name="index.html" logo={props.xampp_logo}  route="http://localhost/php-test/index.php">
+            </Result>
+            <h2 className="title-h2" id="SESSION">9 - <bdi>$_SESSION</bdi></h2>
+            <p className="style_divv"></p>
+            <div className="mital"> متال : </div>
+            <CodeHighlighter code={``} language="php" file_name="C:\xampp\htdocs\php-test\index.php" addclassName="mt-3 mb-3" copie={true}/>
+            <Result title="localhost/php-test/index.php" file_name="index.html" logo={props.xampp_logo}  route="http://localhost/php-test/index.php">
+            </Result>
         </article>
     </>
     )
