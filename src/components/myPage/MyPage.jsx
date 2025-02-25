660b52971833583b4e3cdb18f0ea00ec716a9c11
RedaFarissi <redaesskouni@gmail.com>
2025-02-25 22:22:43 +0000

    .

--- src/components/myPage/MyPage.jsx
+++ src/components/myPage/MyPage.jsx
@@ -1,24 +1,32 @@
 import { useState, useEffect } from "react";
 import "./MyPage.sass";
-
+import { Footer } from "../path";
+import { useNavigate } from "react-router-dom";
 
 const MyPage = (props) => {
+    const navigate = useNavigate();
+  
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
 
-    //const [user, setUser] = useState(null);
     const [oldPassword, setOldPassword] = useState("");
     const [newPassword, setNewPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [message, setMessage] = useState("");
-
+   
+    
     useEffect(() => {
         const fetchUser = async () => {
             setLoading(true);
             try {
                 const token = localStorage.getItem("auth_token");
+                if (!token) {
+                  navigate("/login"); // Redirect if no token
+                  return;
+                }
+             
                 const response = await fetch("http://localhost:8000/api/user/", {
                     method: "GET",
                     headers: {
@@ -39,9 +47,10 @@ const MyPage = (props) => {
                 setLoading(false);
             }
         };
-
+        
         fetchUser();
-    }, []);
+    }, [navigate]);
+
     if (loading) return <p style={{marginTop: "200px"}} > Loading user info...</p>;
     if (error) return <p style={{marginTop: "200px"}} >Error: {error}</p>;
 
@@ -82,15 +91,19 @@ const MyPage = (props) => {
     };
   
     return (
+<>
     <section  className="container" style={{marginTop: "200px", textAlign: "right"}} onClick={props.boxProfileStyle}>
       <div className="my-page-parent">
-        <div className="my-page-child">
-            <h2 className="font-bold mb-4 font-family-cursive">ملف تعريف المستخدم</h2>
-            <strong>اسم المستخدم :</strong> {user.username} <br />
-            <strong>بريد إلكتروني :</strong> {user.email}
+        <div className="my-page-child row">
+            <h2 className="font-bold mb-4 font-family-cursive col-sm-12">ملف تعريف المستخدم</h2>
+            <strong className="col-sm-3">اسم المستخدم :</strong> 
+            <div className="col-sm-8 fs-5"> {user.username}   </div>
+            <strong className="col-sm-3">بريد إلكتروني :</strong> 
+            <div  className="col-sm-8 fs-5">{user.email}</div>
         </div>
-        
+        <br />
         <hr />
+        <br />
         <div className="my-page-child">
             <h2 className="font-bold mb-4 font-family-cursive">تغيير كلمة المرور</h2>
             <form onSubmit={handlePasswordChange} className="mt-2 row">
@@ -98,23 +111,23 @@ const MyPage = (props) => {
                   <strong className="col-sm-3 mt-3">  كلمة المرور الحالية : </strong>
                   <input
                     type="password"    value={oldPassword} 
-                    onChange={(e) => setOldPassword(e.target.value)} className="block w-full p-2 border rounded mt-1 col-sm-8"
+                    onChange={(e) => setOldPassword(e.target.value)} className="block w-full p-2 border rounded mt-1 col-sm-7"
                     required
                   />
                   <strong className="col-sm-3 mt-3">كلمة المرور الجديدة :</strong> 
                   <input
                     type="password"    value={newPassword}   
-                    onChange={(e) => setNewPassword(e.target.value)} className="block w-full p-2 border rounded mt-2 col-sm-8"
+                    onChange={(e) => setNewPassword(e.target.value)} className="block w-full p-2 border rounded mt-2 col-sm-7"
                     required
                   />
                   <strong className="col-sm-3 mt-3">تأكيد كلمة المرور الجديدة :</strong>
                   <input
                     type="password"    value={confirmPassword}   
-                    onChange={(e) => setConfirmPassword(e.target.value)}  className="block w-full p-2 border rounded mt-2 col-sm-8"
+                    onChange={(e) => setConfirmPassword(e.target.value)}  className="block w-full p-2 border rounded mt-2 col-sm-7"
                     required
                   />
                   <div className="col-sm-3 mt-3">  </div>
-                  <button type="submit" className="btn border pt-3 pb-3 mt-2 col-sm-8" style={{backgroundColor: "#171c2c",color:"white"}}>  تحديث كلمة المرور  </button>
+                  <button type="submit" className="btn btn-outline-success mt-2 col-sm-7" >  تحديث كلمة المرور  </button>
             </form>
             <div className="text-center mt-4">
                 {message && <p className="mt-2 text-red-500">{message}</p>}
@@ -122,6 +135,14 @@ const MyPage = (props) => {
         </div>
       </div>
     </section>
+    <br /><br />
+    <Footer 
+          widthFooter="100%"
+          block_1={"col-sm-10 col-sm-10 col-md-8 col-lg-4 offset-lg-2 col-xl-4 offset-xl-1"}
+          block_2={"col-sm-10 col-md-8 col-lg-4 col-xl-4"}
+          blockChildStyle={{width: "90%" , margin:"auto"}}
+    />
+</>
     );
 };
 
--- src/components/paypalpayment/PayPalPayment.jsx
+++ src/components/paypalpayment/PayPalPayment.jsx
@@ -1,42 +1,52 @@
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import "./PayPalPayment.sass";
+import { Footer } from "../path";
 
 const PayPalPayment = (props) => {
   return (
-<section className="PayPalPayment-section" onClick={props.boxProfileStyle}>
-  <div className="container  d-flex align-items-center justify-content-center">
-    <div className="row box-payment">
-        <PayPalScriptProvider options={{ "client-id": "AWNVpwOgDcdK7J8lXb8SqSM8-sud2BKP_Vgblzf47NIlZnLLVYRjEoTDhgw1L8RMqOQl7gXlj-xR8B0Z" }}>
-          <PayPalButtons
-            createOrder={(data, actions) => {
-              return actions.order.create({
-                purchase_units: [
-                  {
-                    amount: {
-                      value: "10.00", // Change this to your price
-                    },
-                  },
-                ],
-              });
-            }}
-            onApprove={(data, actions) => {
-              return actions.order.capture().then((details) => {
-                alert(`Transaction completed by ${details.payer.name.given_name}`);
-                // Send transaction details to Django backend
-                fetch("/payment/api/paypal-success/", {
-                  method: "POST",
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                  body: JSON.stringify({ orderID: data.orderID }),
-                });
-              });
-            }}
-          />
-        </PayPalScriptProvider>
-    </div>
-  </div>
-</section>
+<>
+    <section className="PayPalPayment-section" onClick={props.boxProfileStyle}>
+      <div className="container  d-flex align-items-center justify-content-center">
+        <div className="row box-payment">
+            <PayPalScriptProvider options={{ "client-id": "AWNVpwOgDcdK7J8lXb8SqSM8-sud2BKP_Vgblzf47NIlZnLLVYRjEoTDhgw1L8RMqOQl7gXlj-xR8B0Z" }}>
+              <PayPalButtons
+                createOrder={(data, actions) => {
+                  return actions.order.create({
+                    purchase_units: [
+                      {
+                        amount: {
+                          value: "10.00", // Change this to your price
+                        },
+                      },
+                    ],
+                  });
+                }}
+                onApprove={(data, actions) => {
+                  return actions.order.capture().then((details) => {
+                    alert(`Transaction completed by ${details.payer.name.given_name}`);
+                    // Send transaction details to Django backend
+                    fetch("/payment/api/paypal-success/", {
+                      method: "POST",
+                      headers: {
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify({ orderID: data.orderID }),
+                    });
+                  });
+                }}
+              />
+            </PayPalScriptProvider>
+        </div>
+      </div>
+    </section>
+    <br /><br /><br /><br /><br />
+    <Footer 
+          widthFooter="100%"
+          block_1={"col-sm-10 col-sm-10 col-md-8 col-lg-4 offset-lg-2 col-xl-4 offset-xl-1"}
+          block_2={"col-sm-10 col-md-8 col-lg-4 col-xl-4"}
+          blockChildStyle={{width: "90%" , margin:"auto"}}
+    />
+</>
   );
 };
 
